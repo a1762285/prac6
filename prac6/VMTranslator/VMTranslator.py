b7831ae75c6896cb0a713b1721e03425b153ae8e
a1762285 <zhuchenzhuo@icloud.com>
2023-10-19 20:36:46 +1030

    312321

--- prac6/VMTranslator/VMTranslator.py
+++ prac6/VMTranslator/VMTranslator.py
@@ -52,15 +52,15 @@ class VMTranslator:
 
     def vm_label(label):
         '''Generate Hack Assembly code for a VM label operation'''
-        return ""
+        return f"({label})"
 
     def vm_goto(label):
         '''Generate Hack Assembly code for a VM goto operation'''
-        return ""
+        return f"@{label}\n0;JMP\n"
 
     def vm_if(label):
         '''Generate Hack Assembly code for a VM if-goto operation'''
-        return ""
+        return f'@SP\nM=M-1\nA=M\nD=M\n@%s\nD;JNE\n'
 
     def vm_function(function_name, n_vars):
         '''Generate Hack Assembly code for a VM function operation'''
